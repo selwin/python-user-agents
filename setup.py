094c42e86e680ad95052f121ed284e1b1fa62e1b
Selwin Ong <selwin.ong@gmail.com>
2014-07-14 09:00:09 +0700

    Updated README.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['user_agents'],
     url='https://github.com/selwin/python-user-agents',
     license='MIT',
-    description='A library to identify devices (phones, tablets) and their capabilities by parsing (browser) user agent strings',
+    description='A library to identify devices (phones, tablets) and their capabilities by parsing (browser/HTTP) user agent strings',
     long_description=open('README.rst').read(),
     zip_safe=False,
     include_package_data=True,

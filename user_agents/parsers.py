fc88b29aa8b57fbdc921bf7d092a1aac39de1f6f
Sam Sandberg <sam.sandberg@gmail.com>
2014-08-12 11:40:12 -0400

    Added unicode support, version bump

--- user_agents/parsers.py
+++ user_agents/parsers.py
@@ -111,7 +111,10 @@ class UserAgent(object):
         os = ("%s %s" % (self.os.family, self.os.version_string)).strip()
         browser = ("%s %s" % (self.browser.family, self.browser.version_string)).strip()
         return " / ".join([device, os, browser])
-    
+
+    def __unicode__(self):
+        return unicode(str(self))
+
     def _is_android_tablet(self):
         # Newer Android tablets don't have "Mobile" in their user agent string,
         # older ones like Galaxy Tab still have "Mobile" though they're not

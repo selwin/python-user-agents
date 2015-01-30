ec2a0d3154b2bb4bf9a1dc852c802b317905ce63
Selwin Ong <selwin.ong@gmail.com>
2015-01-30 13:35:46 +0700

    Properly identify Google's mobile spider as mobile device.

--- user_agents/parsers.py
+++ user_agents/parsers.py
@@ -177,6 +177,9 @@ class UserAgent(object):
         # TODO: remove after https://github.com/tobie/ua-parser/issues/126 is closed
         if 'J2ME' in self.ua_string or 'MIDP' in self.ua_string:
             return True
+        # This is here mainly to detect Google's Mobile Spider
+        if 'iPhone;' in self.ua_string:
+            return True
         # Mobile Spiders should be identified as mobile
         if self.device.family == 'Spider' and 'Mobile' in self.browser.family:
             return True
